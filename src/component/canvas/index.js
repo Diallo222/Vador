dd506e96738bb78ddb64e4f0f5909a1e729dff91
diallo_123 <almahadydiallo@gmail.com>
2023-12-06 17:43:06 +0100

    head moved to y 60

--- src/component/canvas/index.js
+++ src/component/canvas/index.js
@@ -2,11 +2,9 @@ import { Vador } from "./Vador";
 import Loader from "./Loader";
 import Scene from "./Scene";
 import { Darth } from "./Darth";
-import { Saber } from "./Saber";
 export {
     Vador,
     Loader,
     Scene,
     Darth,
-    Saber
 }
